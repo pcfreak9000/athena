ab0a86cc4d10d3cceb3eea0be7ce40862832deb1
pcfreak9000 <roman.borris@gmail.com>
2024-12-09 14:27:18 +0100

    changed compilejob

--- compilejob.sh
+++ compilejob.sh
@@ -1,10 +1,15 @@
-#PBS -l nodes=1:ppn1=1
+#!/bin/bash
+#PBS -l nodes=1:ppn=14
 #PBS -l walltime=00:10:00
-#PBS -l mem=2gb
+#PBS -l mem=16gb
 #PBS -S /bin/bash
 #PBS -N athena compile job
 #PBS -j oe
-#PBS -o LOG
-
+#PBS -o LOG_COMPILATION
+if [ "$PBS_O_WORKDIR" ] then
+    module load lib/hdf5/1.10.7-gnu-9.2
+    cd $PBS_O_WORKDIR
+fi
+python configure.py -g -b --prob gr_torus --coord=kerr-schild --flux hlle --nghost 4 -hdf5
 make clean
 make -j
