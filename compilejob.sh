41bf4b90f68db3268d2070249684424131aab944
pcfreak9000 <roman.borris@gmail.com>
2024-12-09 15:18:51 +0100

    changed compilejob

--- compilejob.sh
+++ compilejob.sh
@@ -6,10 +6,12 @@
 #PBS -N athena_compile_job
 #PBS -j oe
 #PBS -o LOG_COMPILATION
-#if [ "$PBS_O_WORKDIR" ]; then
-module load lib/hdf5/1.10.7-gnu-9.2
-cd $PBS_O_WORKDIR
-#fi
-python configure.py -g -b --prob gr_torus --coord=kerr-schild --flux hlle --nghost 4 -hdf5
+if [ "$PBS_O_WORKDIR" ]; then
+    cd $PBS_O_WORKDIR
+    module load lib/hdf5/1.10.7-gnu-9.2
+    python configure.py -g -b --prob gr_torus --coord=kerr-schild --flux hlle --nghost 4 -hdf5 --hdf5_path=$HDF5_HOME
+else
+    python configure.py -g -b --prob gr_torus --coord=kerr-schild --flux hlle --nghost 4 -hdf5
+fi
 make clean
 make -j
