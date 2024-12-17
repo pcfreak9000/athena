ad0d0931c785b4ba78748732ce54213165d670b9
pcfreak9000 <roman.borris@gmail.com>
2024-12-17 19:13:46 +0100

    created script for creating an animation

---
+++ create_movie.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+plotcommand="vis/python/plot_spherical.py"
+n_processes=6
+
+# initialize a semaphore with a given number of tokens
+open_sem(){
+    mkfifo pipe-$$
+    exec 3<>pipe-$$
+    rm pipe-$$
+    local i=$1
+    for((;i>0;i--)); do
+        printf %s 000 >&3
+    done
+}
+
+# run the given command asynchronously and pop/push tokens
+run_with_lock(){
+    local x
+    # this read waits until there is something to read
+    read -u 3 -n 3 x && ((0==x)) || exit $x
+    (
+     ( "$@"; )
+    # push the return code of the command to the semaphore
+    printf '%.3d' $? >&3
+    )&
+}
+
+
+mkdir $2 &>/dev/null && echo "Created png directory" || echo "png directory already exists, overriding."
+open_sem $n_processes
+echo "Converting athdf's to png's with $n_processes processes."
+files=($1/*.athdf)
+count_total=${#files[@]}
+count=0
+for filename in $1/*.athdf; do 
+    run_with_lock $plotcommand --logc "${filename}" rho $2/$(basename ${filename}).png
+    count=$((count + 1))
+    percent=$((count * 100 / count_total))
+    #echo $percent
+    printf "\rProgress: \r[%-50s] %d%%" $(head -c $((percent / 2)) < /dev/zero | tr '\0' '#') $percent
+done #| dialog --title "Creating png's from athdf's" --gauge 'Creating pngs...' 6 60 0
+echo  
+echo "Calling ffmpeg, possibly overriding outputfile."
+ffmpeg -framerate $3 -pattern_type glob -i $2/'*.png' \
+  -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" \
+  -c:v libx264 -pix_fmt yuv420p -y out.mp4 &>/dev/null
+echo "Done."
+
