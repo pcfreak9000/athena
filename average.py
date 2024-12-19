fbedc114325cd5ad27bac2a42a00ce94807a8a9a
pcfreak9000 <roman.borris@gmail.com>
2024-12-19 15:46:15 +0100

    stuff

--- average.py
+++ average.py
@@ -11,7 +11,8 @@ import h5py
 import athena_read
 
 def main(**kwargs):
-    
+    #read files one by one and accumulate average of the fields we are interested in
+    #write output file where fields which were not averaged are taken from the first file or left empty?
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
