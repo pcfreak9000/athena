fb37303a5d9922f04824271c7db36fbec79d3eb7
pcfreak9000 <roman.borris@gmail.com>
2025-01-28 18:38:18 +0100

    stuff

--- startjob.sh
+++ startjob.sh
@@ -15,14 +15,14 @@ fi
 
 TEMP=$(grep -F 'num_threads' $ATHENA_CONFIG_FILE)
 export OMP_NUM_THREADS=${TEMP#*=}
-
+echo $OMP_NUM_THREADS
 if [ "$PBS_O_WORKDIR" ]; then
     module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
     module load mpi/openmpi/4.1-gnu-9.2-cuda-11.4
     TARGETDIR=/beegfs/work/tu_zxorf45/$(date +"%Y-%m-%d_%H-%M-%S")
     mkdir -p $TARGETDIR
     cp $ATHENA_CONFIG_FILE $TARGETDIR/
-    mpirun --bind-to core --map-by socket:PE=$OMP_NUM_THREADS -report-bindings bin/athena -i $ATHENA_CONFIG_FILE -d $TARGETDIR
+    mpirun --bind-to core --map-by socket:PE="$OMP_NUM_THREADS" -report-bindings bin/athena -i $ATHENA_CONFIG_FILE -d $TARGETDIR
 else
     TARGETDIR=Gartenzwerg-$(date +"%Y-%m-%d_%H-%M-%S")
     mkdir -p $TARGETDIR
