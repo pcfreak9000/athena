8b61b06194b5f6e3a2aa78df45dad7a8e07d9024
pcfreak9000 <roman.borris@gmail.com>
2025-01-28 13:17:05 +0100

    stuff

--- startjob.sh
+++ startjob.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=14
-#PBS -l walltime=00:10:00
+#PBS -l nodes=1:ppn=4
+#PBS -l walltime=00:15:00
 #PBS -l mem=16gb
 #PBS -S /bin/bash
 #PBS -N athena_test_job
 #PBS -j oe
-#PBS -o LOG_COMPILATION
+#PBS -o LOG_TEST
 
 export OMP_NUM_THREADS=4
-$PBS_O_WORKDIR/bin/athena -i athinput.master_project -d /beegfs/work/tu_zxorf45/$(date +"%Y-%m-%d_%H-%M-%S")
+if [ "$PBS_O_WORKDIR" ]; then
+    cd $PBS_O_WORKDIR
+    module load lib/hdf5/1.10.7-gnu-9.2
+    bin/athena -i athinput.master_project -d /beegfs/work/tu_zxorf45/$(date +"%Y-%m-%d_%H-%M-%S")
+fi
