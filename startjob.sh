0f9b1553fb7dd6d9f432510369b81878f6d85e25
pcfreak9000 <roman.borris@gmail.com>
2025-02-07 16:10:12 +0100

    stuff

--- startjob.sh
+++ startjob.sh
@@ -19,6 +19,9 @@ fi
 if [ -z "${ATHENA_CONFIG_FILE+x}" ]; then
     exit 1
 fi
+if [ -z "${TIMELIMIT_RSTFILE+x}" ]; then
+    exit 1
+fi
 
 
 cd $WORKDIR
@@ -31,7 +34,7 @@ export OMP_NUM_THREADS=${TEMP#*=}
 if [ "$PBS_O_WORKDIR" ]; then
     module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
     module load mpi/openmpi/4.1-gnu-9.2-cuda-11.4
-    mpirun --bind-to core --map-by core -report-bindings $ATHENABIN -i $ATHENA_CONFIG_FILE -d $ATHENA_OUTDIR
+    mpirun --bind-to core --map-by core -report-bindings $ATHENABIN -i $ATHENA_CONFIG_FILE -d $ATHENA_OUTDIR -t $TIMELIMIT_RSTFILE
 else
-    $ATHENABIN -i $ATHENA_CONFIG_FILE -d $ATHENA_OUTDIR
+    $ATHENABIN -i $ATHENA_CONFIG_FILE -d $ATHENA_OUTDIR -t $TIMELIMIT_RSTFILE
 fi
--- startq.sh
+++ startq.sh
@@ -2,6 +2,8 @@
 
 MYNAME=AthenaPP_S19_$(date +"%Y-%m-%d_%H-%M-%S")
 
+TIMELIMIT_RSTFILE=100000:00:00
+
 ATHENA_CONFIG_FILE=athinput.master_project
 
 if [ "$BINAC" ]; then
@@ -12,6 +14,7 @@ fi
 export ATHENABIN=$WORKDIR/athena
 export WORKDIR
 export ATHENA_CONFIG_FILE
+export TIMELIMIT_RSTFILE
 
 mkdir -p $WORKDIR
 cp $ATHENA_CONFIG_FILE $WORKDIR/
@@ -20,7 +23,7 @@ cp bin/athena $WORKDIR/
 
 
 if [ "$BINAC" ]; then
-    qsub -q short -l walltime=00:30:00 -l nodes=1:ppn=4 -l pmem=768mb -N $MYNAME -o $WORKDIR/LOG_ATHENA -v WORKDIR="$WORKDIR",ATHENA_CONFIG_FILE="$ATHENA_CONFIG_FILE",ATHENABIN="$ATHENABIN" $WORKDIR/startjob.sh
+    qsub -q short -l walltime=00:30:00 -l nodes=1:ppn=4 -l pmem=768mb -N $MYNAME -o $WORKDIR/LOG_ATHENA -v WORKDIR="$WORKDIR",ATHENA_CONFIG_FILE="$ATHENA_CONFIG_FILE",ATHENABIN="$ATHENABIN",TIMELIMIT_RSTFILE="$TIMELIMIT_RSTFILE" $WORKDIR/startjob.sh
 else
     $WORKDIR/startjob.sh 2>&1 | tee $WORKDIR/LOG_ATHENA
 fi
