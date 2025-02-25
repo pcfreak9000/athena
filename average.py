ece4be7ba0303faa540a3b77bf5ce916d861d423
pcfreak9000 <roman.borris@gmail.com>
2025-02-25 14:46:11 +0100

    stuff

--- average.py
+++ average.py
@@ -13,9 +13,12 @@ import athena_read
 def main(**kwargs):
     #read files one by one and accumulate average of the fields we are interested in
     #write output file where fields which were not averaged are taken from the first file or left empty?
+    data = athena_read.athdf(kwargs['data_file'])
+    print(data)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('data_file')
     
     args = parser.parse_args()
     main(**vars(args))
