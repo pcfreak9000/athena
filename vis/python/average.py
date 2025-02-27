a286d65ac3b04c2c9b1cda15ade712f9bb9cea20
pcfreak9000 <roman.borris@gmail.com>
2025-02-27 15:27:48 +0100

    stuff

--- vis/python/average.py
+++ vis/python/average.py
@@ -27,11 +27,11 @@ def main(**kwargs):
         files.append(basename + str(i).zfill(zfi) + suffix)
         #print(files[i-start])
         
-    basedata = athena_read.athdf(files[0], return_levels=True, raw=True)
+    basedata = athena_read.athdf(files[0])
     count = 1
     for file in files[1:]:
         count = count + 1 
-        data = athena_read.athdf(file, return_levels=True, raw=True)
+        data = athena_read.athdf(file)
         for datasetName in toavg:
             shape = data[datasetName].shape
             #print(shape)
@@ -39,10 +39,9 @@ def main(**kwargs):
             for i in range(shape[0]):
                 for j in range(shape[1]):
                     for k in range(shape[2]):
-                        for a in range(shape[3]):
-                            value = data[datasetName][i,j,k,a]
-                            curavg = basedata[datasetName][i,j,k,a]
-                            basedata[datasetName][i,j,k,a] = curavg * (count-1)/count + value / count
+                        value = data[datasetName][i,j,k]
+                        curavg = basedata[datasetName][i,j,k]
+                        basedata[datasetName][i,j,k] = curavg * (count-1)/count + value / count
         del data # lets free some memory, even though technically this should be done automatically by python????
 
     # here we need to write the basedata again to a new athdf file
@@ -51,7 +50,7 @@ def main(**kwargs):
     with h5py.File(files[0], 'r') as f:
         attributes = f.attrs.items()
         attrs = dict(attributes)
-        print(f['x1f'].shape)
+        #attrs['RootGridSize'] = 
         #ll = f['LogicalLocations'][:]
     with h5py.File(outfile, 'w') as f:
         for k,v in attrs.items():
@@ -62,26 +61,29 @@ def main(**kwargs):
         nx1 = attrs['RootGridSize'][0]
         nx2 = attrs['RootGridSize'][1]
         nx3 = attrs['RootGridSize'][2]
-        f.create_dataset('Levels', data=basedata['Levels'], dtype='>i4')
-        f.create_dataset('LogicalLocations', data=basedata['LogicalLocations'], dtype='>i8')
-        f.create_dataset('x1f', data=basedata['x1f'], dtype='>f4')
-        f.create_dataset('x2f', data=basedata['x2f'], dtype='>f4')
-        f.create_dataset('x3f', data=basedata['x3f'], dtype='>f4')
-        f.create_dataset('x1v', data=basedata['x1v'], dtype='>f4')
-        f.create_dataset('x2v', data=basedata['x2v'], dtype='>f4')
-        f.create_dataset('x3v', data=basedata['x3v'], dtype='>f4')
+        print(nx1)
+        print(nx2)
+        print(nx3)
+        data = basedata
+        # Write datasets
+        f.create_dataset('Levels', data=[0], dtype='>i4')
+        f.create_dataset('LogicalLocations', data=[0, 0, 0], dtype='>i8',
+                         shape=(1, 3))
+        f.create_dataset('x1f', data=data['x1f'], dtype='>f4', shape=(1, nx1 + 1))
+        f.create_dataset('x2f', data=data['x2f'], dtype='>f4', shape=(1, nx2 + 1))
+        f.create_dataset('x3f', data=data['x3f'], dtype='>f4', shape=(1, nx3 + 1))
+        f.create_dataset('x1v', data=data['x1v'], dtype='>f4', shape=(1, nx1))
+        f.create_dataset('x2v', data=data['x2v'], dtype='>f4', shape=(1, nx2))
+        f.create_dataset('x3v', data=data['x3v'], dtype='>f4', shape=(1, nx3))
         var_offset = 0
-        #nx1 = attrs['RootGridSize'][0]
-        #nx2 = attrs['RootGridSize'][1]
-        #nx3 = attrs['RootGridSize'][2]
         for dataset_name, num_vars in zip(
-            f.attrs['DatasetNames'], f.attrs['NumVariables']):
+                f.attrs['DatasetNames'], f.attrs['NumVariables']):
             f.create_dataset(dataset_name.decode('ascii', 'replace'), dtype='>f4',
-                    shape=(num_vars, basedata.shape[0], basedata.shape[1], basedata.shape[2], basedata.shape[3]))
+                             shape=(num_vars, 1, nx3, nx2, nx1))
             for var_num in range(num_vars):
                 variable_name = f.attrs['VariableNames'][var_num + var_offset]
                 variable_name = variable_name.decode('ascii', 'replace')
-                f[dataset_name][var_num, :, :, :, :] = basedata[variable_name]
+                f[dataset_name][var_num, 0, :, :, :] = data[variable_name]
             var_offset += num_vars
 
 
