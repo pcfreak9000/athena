13752fdd57b56c7d6492222820e4a7e9e6b9e9dd
pcfreak9000 <roman.borris@gmail.com>
2025-04-16 17:23:40 +0200

    fix

--- movieq.sh
+++ movieq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MYNAME="$1"
-PLOT_ARGS="$2"
+PLOT_OPTIONS="$2"
 
 
 if [ "$BINAC" ]; then
@@ -11,14 +11,14 @@ else
 fi
 export WORKDIR
 export ATHENA_GIT_DIR=$(pwd)
-export PLOT_ARGS
+export PLOT_OPTIONS
 
 cp moviejob.sh $WORKDIR/
 cp create_movie.sh $WORKDIR/
 
 
 if [ "$BINAC" ]; then
-    qsub -q tiny -l walltime=00:20:00 -l nodes=1:ppn=24 -l mem=12gb -N MOV_$MYNAME -o $WORKDIR/LOG_MOVIE -v WORKDIR="$WORKDIR",ATHENA_GIT_DIR="$ATHENA_GIT_DIR",PLOT_ARGS="$PLOT_ARGS" $WORKDIR/moviejob.sh
+    qsub -q tiny -l walltime=00:20:00 -l nodes=1:ppn=24 -l mem=12gb -N MOV_$MYNAME -o $WORKDIR/LOG_MOVIE -v WORKDIR="$WORKDIR",ATHENA_GIT_DIR="$ATHENA_GIT_DIR",PLOT_OPTIONS="$PLOT_OPTIONS" $WORKDIR/moviejob.sh
 else
     $WORKDIR/moviejob.sh 2>&1 | tee $WORKDIR/LOG_MOVIE
 fi
