dca58346314c71cd4cfe769497af5e9c4d8d32b1
pcfreak9000 <roman.borris@gmail.com>
2025-04-16 16:11:40 +0200

    changed movie script

--- create_movie.sh
+++ create_movie.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-plotcommand="$5/vis/python/plot_spherical.py"
-n_processes=20
+ath_git_dir=$5
+athdf_dir=$1
+png_dir=$2
+fps=$3
+file_out=$4
+args=$6
+hideprogress=$7
+
+plotcommand="$ath_git_dir/vis/python/plot_spherical.py"
+n_processes=${P9000_WORKER_THREADS:-4}
 
 # initialize a semaphore with a given number of tokens
 open_sem(){
@@ -27,27 +35,25 @@ run_with_lock(){
 }
 
 pwd
-mkdir $2 &>/dev/null && echo "Created png directory." || echo "png directory already exists, overriding. Or there is an error..."
+mkdir $png_dir &>/dev/null && echo "Created png directory." || echo "png directory already exists, overriding. Or there is an error..."
 open_sem $n_processes
 echo "Converting athdf's to png's with $n_processes processes."
-files=($1/*.athdf)
+files=($athdf_dir/*.athdf)
 count_total=${#files[@]}
 count=0
-for filename in $1/*.athdf; do 
-    if [ -z "${FIELDLINES_STREAM+x}" ]; then 
-        run_with_lock $plotcommand "${filename}" rho $2/$(basename ${filename}).png
-    else
-        run_with_lock $plotcommand --stream Bcc --logc "${filename}" rho $2/$(basename ${filename}).png
-    fi
+for filename in $athdf_dir/*.athdf; do 
+    run_with_lock $plotcommand "${filename}" $args $png_dir/$(basename ${filename}).png
     count=$((count + 1))
     percent=$((count * 100 / count_total))
     #echo $percent
-    printf "\r[%-50s] %d%%" $(head -c $((percent / 2)) < /dev/zero | tr '\0' '#') $percent
+    if [ "$hideprogress" = "" ]; then
+        printf "\r[%-50s] %d%%" $(head -c $((percent / 2)) < /dev/zero | tr '\0' '#') $percent
+    fi
 done #| dialog --title "Creating png's from athdf's" --gauge 'Creating pngs...' 6 60 0
 echo  
 echo "Calling ffmpeg, possibly overriding outputfile."
-ffmpeg -framerate $3 -pattern_type glob -i $2/'*.png' \
+ffmpeg -framerate $fps -pattern_type glob -i $png_dir/'*.png' \
   -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" \
-  -c:v libx264 -pix_fmt yuv420p -y $4/out.mp4 2>/dev/null
+  -c:v libx264 -pix_fmt yuv420p -y $file_out 2>/dev/null
 echo "Done."
 
--- moviejobn.sh
+++ moviejobn.sh
@@ -3,6 +3,8 @@
 #PBS -j oe
 #PBS -m a
 
+export P9000_WORKER_THREADS=$PBS_WORKER_THREADS
+
 ATHDF_DIR=$WORKDIR/simout
 
 module load lib/ffmpeg/3.4.7
@@ -11,4 +13,4 @@ module load lib/ffmpeg/3.4.7
 source $HOME/miniconda3/etc/profile.d/conda.sh
 conda activate master_project_env
 
-$WORKDIR/create_movie.sh $ATHDF_DIR $WORKDIR/pngs 25 $WORKDIR $ATHENA_GIT_DIR
+$WORKDIR/create_movie.sh $ATHDF_DIR $WORKDIR/pngs 25 $WORKDIR/out.mp4 $ATHENA_GIT_DIR "rho --logc" noprogress
