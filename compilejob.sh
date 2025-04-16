c1576dc7c5eaa33e12f3043276da0cc7a5e8f077
pcfreak9000 <roman.borris@gmail.com>
2025-04-16 16:43:23 +0200

    changed scripts again

--- compilejob.sh
+++ compilejob.sh
@@ -2,12 +2,13 @@
 #PBS -l nodes=1:ppn=20
 #PBS -l walltime=00:05:00
 #PBS -l mem=16gb
+#PBS -q tiny
 #PBS -S /bin/bash
 #PBS -N athena_compile_job
 #PBS -j oe
 #PBS -o LOG_COMPILATION
 
-PBS_WORKER_THREADS=20
+PBS_WORKER_THREADS=$PBS_NP
 
 ATHENA_CONFIG_FILE=athinput.master_project
 
@@ -15,7 +16,7 @@ if [ "$PBS_O_WORKDIR" ]; then
     cd $PBS_O_WORKDIR
 fi
 
-TEMP=$(grep -F 'num_threads' $ATHENA_CONFIG_FILE)
+TEMP=$(grep -F 'num_threads' "$ATHENA_CONFIG_FILE")
 #is this even neccessary for compilation???
 export OMP_NUM_THREADS=${TEMP#*=}
 
@@ -23,7 +24,7 @@ if [ "$PBS_O_WORKDIR" ]; then
     export P9000_WORKER_THREADS=$PBS_WORKER_THREADS
     module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
     module load mpi/openmpi/4.1-gnu-9.2-cuda-11.4
-    python configure.py -g -b -mpi --prob gr_torus --coord=kerr-schild --flux hlle --nghost 4 -hdf5 --hdf5_path=$HDF5_HOME
+    python configure.py -g -b -mpi --prob gr_torus --coord=kerr-schild --flux hlle --nghost 4 -hdf5 --hdf5_path="$HDF5_HOME"
 else
     python configure.py -g -b -omp --prob gr_torus --coord=kerr-schild --flux hlle --nghost 4 -hdf5
 fi
