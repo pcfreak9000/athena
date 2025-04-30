8cc8bad8eced78f0882f23c6b90908850e863cce
pcfreak9000 <roman.borris@gmail.com>
2025-04-30 14:07:06 +0200

    utility scripts

---
+++ mdq2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+WORKDIR="$1"
+WORKDIR="${WORKDIR%/}"
+myname=$(basename "$WORKDIR")
+
+export WORKDIR
+
+#echo "$WORKDIR"
+
+if [ "$BINAC2" ]; then
+    sbatch --partition compute -t 2:00:00 -N 1 --ntasks-per-node=1 --mem=32g -J MDOT_"$myname" --output="$WORKDIR"/LOG_MDOT --error="$WORKDIR"/LOG_MDOT --export=ALL "$HOME"/athena/mdotjob2.sh
+else
+    ./mdotjob2.sh 2>&1 | tee "$WORKDIR"/LOG_MDOT
+fi
