0a51f3a0f7fd6044899dd69c075dd55b79f83d2e
pcfreak9000 <roman.borris@gmail.com>
2025-04-30 14:27:31 +0200

    multiprocessing for mdot script

--- vis/python/mdot.py
+++ vis/python/mdot.py
@@ -5,6 +5,7 @@ import argparse
 import athena_read
 import math
 import csv
+import multiprocessing
 
 def integrate_flow(rho, velr, thcoords, thbord, radius):
     flow = 0.0
@@ -13,28 +14,35 @@ def integrate_flow(rho, velr, thcoords, thbord, radius):
         dS = radius*radius * math.sin(thcoords[thetaInd]) * dtheta #does this make sense????
         flow += - 2 * math.pi * rho[0, thetaInd, 0] * velr[0, thetaInd, 0] * dS
     return flow
-    
+
+def eval(input_filename):    
+    data = athena_read.athdf(input_filename)
+    rho = data['rho']
+    velr = data['vel1']
+    thcoords = data['x2v']
+    thbord = data['x2f']
+    rcoords = data['x1v']
+    flow = integrate_flow(rho, velr, thcoords, thbord, rcoords[0])
+    print(input_filename)
+    return (data['Time'], flow)
+     
 def main(**kwargs):
     if kwargs['end'] < kwargs['start']:
         raise Exception("end < start")
     file_nums = range(kwargs['start'], kwargs['end']+1, kwargs['stride'])
     flows = []
     times = []
-    count = 0
+    pool = multiprocessing.Pool(64)
+    input_filenames = []
     for n in file_nums:
-        count = count + 1
         #input_filename = kwargs['input_filename']
-        input_filename = '{0}.{1:05d}.athdf'.format(kwargs['input_filename'], n)
-        data = athena_read.athdf(input_filename)
-        rho = data['rho']
-        velr = data['vel1']
-        thcoords = data['x2v']
-        thbord = data['x2f']
-        rcoords = data['x1v']
-        flow = integrate_flow(rho, velr, thcoords, thbord, rcoords[0])
-        flows.append(flow)
-        times.append(data['Time'])
-        print(count/len(file_nums))
+        input_filenames.append('{0}.{1:05d}.athdf'.format(kwargs['input_filename'], n))
+    results = pool.map(eval, input_filenames)
+    for r in results:
+        times.append(r[0])
+        flows.append(r[1])
+    pool.close()
+    pool.terminate()
     lis = [times, flows]
     zl = zip(*lis)
     with open(kwargs['output_filename'], 'w', newline='') as csvfile:
