0417965f054a17ba20f276e98aea3f86d53acc6b
pcfreak9000 <roman.borris@gmail.com>
2025-05-06 16:32:59 +0200

    script changes

--- vis/python/mdot.py
+++ vis/python/mdot.py
@@ -7,22 +7,24 @@ import math
 import csv
 import multiprocessing
 
-def integrate_flow(rho, velr, thcoords, thbord, radius):
+def integrate_flow(rho, velr, thcoords, thbord, radius, a):
     flow = 0.0
     for thetaInd in range(len(thcoords)):
         dtheta = thbord[thetaInd + 1] - thbord[thetaInd]
-        dS = radius*radius * math.sin(thcoords[thetaInd]) * dtheta #does this make sense????
+	costh = math.cos(thcoords[thetaInd])
+        dS = (radius*radius + a*a*costh*costh) * math.sin(thcoords[thetaInd]) * dtheta
         flow += 2 * math.pi * rho[0, thetaInd, 0] * velr[0, thetaInd, 0] * dS
     return flow
 
 def eval(input_filename):    
-    data = athena_read.athdf(input_filename)
+    data = athena_read.athdf(input_filename[0])
+    a = input_filename[1]
     rho = data['rho']
     velr = data['vel1']
     thcoords = data['x2v']
     thbord = data['x2f']
     rcoords = data['x1v']
-    flow = integrate_flow(rho, velr, thcoords, thbord, rcoords[0])
+    flow = integrate_flow(rho, velr, thcoords, thbord, rcoords[0], a)
     print(input_filename)
     return (data['Time'], flow)
      
@@ -36,7 +38,7 @@ def main(**kwargs):
     input_filenames = []
     for n in file_nums:
         #input_filename = kwargs['input_filename']
-        input_filenames.append('{0}.{1:05d}.athdf'.format(kwargs['input_filename'], n))
+        input_filenames.append(('{0}.{1:05d}.athdf'.format(kwargs['input_filename'], n), kwargs['a']))
     results = pool.map(eval, input_filenames)
     for r in results:
         times.append(r[0])
@@ -70,5 +72,8 @@ if __name__ == '__main__':
                         type=int,
                         default=0,
                         help='stride in file numbers to be converted')
+    parser.add_argument('a',
+			type=float,
+			help='BH spin parameter')
     args = parser.parse_args()
     main(**vars(args))
