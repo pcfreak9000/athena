5f60fc224f6e6e212d4385f1b1c14d126a438522
pcfreak9000 <roman.borris@gmail.com>
2025-05-22 12:15:25 +0200

    moved old scripts

--- anaq2.sh
+++ anaq2.sh
@@ -5,6 +5,6 @@ spin="$2"
 
 ./movieq2.sh "$file" "q1 --vmin=0.0 --vmax=12.0 --abs" "q1"
 ./movieq2.sh "$file" "q2 --vmin=0.0 --vmax=12.0 --abs" "q2"
-./movieq2.sh "$file" "q3 --vmin=0.0 --vmax=24.0 --abs" "q3"
+#./movieq2.sh "$file" "q3 --vmin=0.0 --vmax=24.0 --abs" "q3"
 ./movieq2.sh "$file" "rho --vmin=0.0 --vmax=1.0"
 ./mdq2.sh "$file" "$spin"
