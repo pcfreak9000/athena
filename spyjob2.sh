ae615b025e68c42eb6568f078d04785d5c6ec169
pcfreak9000 <roman.borris@gmail.com>
2025-05-23 14:08:56 +0200

    fix?

--- spyjob2.sh
+++ spyjob2.sh
@@ -16,7 +16,7 @@ myname=$(basename "$WORKDIR")
 scriptname=$(basename "$SCRIPT")
 
 if [ "$BINAC2" ]; then
-    sbatch --partition compute -t 00:30:00 -N 1 --ntasks-per-node=$tasks --mem=32g -J "$scriptname"_"$myname" --output="$WORKDIR"/LOGO_"$scriptname" --error="$WORKDIR"/LOGE_"$scriptname" --export=ALL "$SCRIPT" 
+    sbatch --partition compute -t 00:30:00 -N 1 --ntasks-per-node=$tasks --mem=32g -J "$scriptname"_"$myname" --output="$WORKDIR"/LOGO_"$scriptname" --error="$WORKDIR"/LOGE_"$scriptname" --export=ALL pyjob2.sh 
 else
     eval "$SCRIPT" $SCRIPT_ARGS 2>&1 | tee "$WORKDIR"/LOG_"$scriptname"
 fi
