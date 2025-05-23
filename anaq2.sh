79babc98b9540cf16dd8359412d061380463166c
pcfreak9000 <roman.borris@gmail.com>
2025-05-23 15:15:14 +0200

    added time to anaq2

--- anaq2.sh
+++ anaq2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 file="$1"
-#spin="$2"
 maxf="$3"
 minf="$2"
 
@@ -13,6 +12,6 @@ spin=$(grep -E "^\s*a\s*=" "$athfile" | awk -F'=' '{print $2}' | awk '{print $1}
 ./movieq2.sh "$file" "q2 --vmin=0.0 --vmax=12.0 --abs" "q2"
 ##./mov3eq2.sh "$file" "q3 --vmin=0.0 --vmax=24.0 --abs" "q3"
 ./movieq2.sh "$file" "rho --vmin=0.0 --vmax=1.0"
-./spyjob2.sh vis/python/mdot.py "$file" 64 '$WORKDIR/simout/master_project.prim $WORKDIR/mdot.csv' "$minf $maxf 1" "$spin"
-./spyjob2.sh vis/python/timeaverage.py "$file" 1 '$WORKDIR/simout/master_project.prim $WORKDIR/tavg.athdf' "$minf $maxf 1 -q rho -q u0 -q u1 -q u2 -q u3"
-./spyjob2.sh vis/python/spaceaverage.py "$file" 64 '$WORKDIR/simout/master_project.prim $WORKDIR/savg.csv' "$minf $maxf 1 --lvl=2 --max=20 -q q1 -q q2 -q q3"
+./spyjob2.sh vis/python/mdot.py "$file" 64 00:20:00 '$WORKDIR/simout/master_project.prim $WORKDIR/mdot.csv' "$minf $maxf 1" "$spin"
+./spyjob2.sh vis/python/timeaverage.py "$file" 1 3:00:00 '$WORKDIR/simout/master_project.prim $WORKDIR/tavg.athdf' "$minf $maxf 1 -q rho -q u0 -q u1 -q u2 -q u3"
+./spyjob2.sh vis/python/spaceaverage.py "$file" 64 00:20:00 '$WORKDIR/simout/master_project.prim $WORKDIR/savg.csv' "$minf $maxf 1 --lvl=2 --max=20 -q q1 -q q2 -q q3"
