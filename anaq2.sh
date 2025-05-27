6e1360a46e399e9b0e3f1c6aa4812b4e96f60988
zxorf45@uni-tuebingen.de <tu_zxorf45@login01.cluster>
2025-05-27 11:37:02 +0200

    fix 2

--- anaq2.sh
+++ anaq2.sh
@@ -13,5 +13,5 @@ spin=$(grep -E "^\s*a\s*=" "$athfile" | awk -F'=' '{print $2}' | awk '{print $1}
 #./mov3eq2.sh "$file" "q3 --vmin=0.0 --vmax=24.0 --abs" "q3"
 ./movieq2.sh "$file" "rho --vmin=0.0 --vmax=1.0"
 ./spyjob2.sh vis/python/mdot.py "$file" 64 00:20:00 '$WORKDIR/simout/master_project.prim $WORKDIR/mdot.csv' "$minf $maxf 1" "$spin"
-./spyjob2.sh vis/python/timeaverage.py "$file" 1 3:00:00 '$WORKDIR/simout/master_project.prim $WORKDIR/tavg.athdf' "$minf $maxf 1 -q rho u0 u1 u2 u3 q1 q2 q3"
+./spyjob2.sh vis/python/timeaverage.py "$file" 1 3:00:00 '$WORKDIR/simout/master_project.prim $WORKDIR/tavg.athdf' "$minf $maxf 1 --nanzero -q rho u0 u1 u2 u3 q1 q2 q3"
 ./spyjob2.sh vis/python/spaceaverage.py "$file" 64 00:20:00 '$WORKDIR/simout/master_project.prim $WORKDIR/savg.csv' "$minf $maxf 1 --lvl=2 --max=20 -q q1 q2 q3"
