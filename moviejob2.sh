a0d77a244d719749291e950cf7dd75960072fb32
pcfreak9000 <roman.borris@gmail.com>
2025-07-23 14:30:26 +0200

    changed movie script

--- moviejob2.sh
+++ moviejob2.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-
-export P9000_WORKER_THREADS=$SLURM_JOB_CPUS_PER_NODE
+if [ "$BINAC2" ]; then
+    export P9000_WORKER_THREADS=$SLURM_JOB_CPUS_PER_NODE
+fi
 
 ATHDF_DIR="$WORKDIR"/simout
 
-module load vis/ffmpeg/ffmpeg-5.1
-
-
-source "$HOME"/miniconda3/etc/profile.d/conda.sh
-conda activate master_project_env
+if [ "$BINAC2" ]; then
+    module load vis/ffmpeg/ffmpeg-5.1
+    source "$HOME"/miniconda3/etc/profile.d/conda.sh
+    conda activate master_project_env
+    nop=noprogress
+fi
 
 if [ -z "$MNAME" ]; then
     MNAME=out
 fi
 
-"$WORKDIR"/create_movie.sh "$ATHDF_DIR" "$WORKDIR"/pngs_"$MNAME" 25 "$WORKDIR"/"$MNAME".mp4 "$ATHENA_GIT_DIR" "$PLOT_OPTIONS" noprogress
+
+
+"$WORKDIR"/create_movie.sh "$ATHDF_DIR" "$WORKDIR"/pngs_"$MNAME" 25 "$WORKDIR"/"$MNAME".mp4 "$ATHENA_GIT_DIR" "$PLOT_OPTIONS" $nop
