3976bb12d238000d177e2441e5304b9a2ecebdf6
pcfreak9000 <roman.borris@gmail.com>
2025-10-06 14:18:32 +0200

    fixed anaq2

--- anaq2.sh
+++ anaq2.sh
@@ -8,6 +8,18 @@ minf="$2"
 athfile="$file/athinput.pp_master_project"
 spin=$(grep -E "^\s*a\s*=" "$athfile" | awk -F'=' '{print $2}' | awk '{print $1}')
 
+refenabled=$(grep -m1 -E '^\s*refinement\s*=' "$athfile" | awk -F'=' '{print $2}' | awk '{print $1}')
+lvlmax=$(grep -E '^\s*level\s*=' "$athfile" \
+    | awk -F'=' '{print $2}' \
+    | awk '{print $1}' \
+    | sort -nr | head -1)
+
+if [ "$refenabled" = "none" ]; then
+    lvlvar=0
+else
+    lvlvar=$lvlmax
+fi
+
 ./movieq2.sh "$file" "q1 --vmin=0.0 --vmax=12.0 --abs" "q1"
 ./movieq2.sh "$file" "q2 --vmin=0.0 --vmax=12.0 --abs" "q2"
 #./mov3eq2.sh "$file" "q3 --vmin=0.0 --vmax=24.0 --abs" "q3"
@@ -17,4 +29,4 @@ spin=$(grep -E "^\s*a\s*=" "$athfile" | awk -F'=' '{print $2}' | awk '{print $1}
 ./movieq2.sh "$file" "rho --logc -r 10" "ii_log"
 ./spyjob2.sh vis/python/mdot.py "$file" 64 00:20:00 '$WORKDIR/simout/master_project.prim $WORKDIR/mdot.csv' "$minf $maxf 1" "$spin"
 ./spyjob2.sh vis/python/timeaverage.py "$file" 1 3:00:00 '$WORKDIR/simout/master_project.prim $WORKDIR/tavg.athdf' "$minf $maxf 1 --nanzero -q rho u0 u1 u2 u3 q1 q2 q3"
-./spyjob2.sh vis/python/spaceaverage.py "$file" 64 00:20:00 '$WORKDIR/simout/master_project.prim $WORKDIR/savg.csv' "$minf $maxf 1 --lvl=2 --max=20 -q q1 q2 q3"
+./spyjob2.sh vis/python/spaceaverage.py "$file" 64 00:20:00 '$WORKDIR/simout/master_project.prim $WORKDIR/savg.csv' "$minf $maxf 1 --lvl=$lvlvar --max=20 -q q1 q2 q3"
