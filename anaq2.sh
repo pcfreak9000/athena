9a4fb77e4ef408a839993a77dfd88578ba7fe470
pcfreak9000 <roman.borris@gmail.com>
2025-10-14 14:30:49 +0200

    changed analysis scripts

--- anaq2.sh
+++ anaq2.sh
@@ -20,8 +20,8 @@ else
     lvlvar=$lvlmax
 fi
 
-./movieq2.sh "$file" "q1 --vmin=0.0 --vmax=12.0 --abs" "q1"
-./movieq2.sh "$file" "q2 --vmin=0.0 --vmax=12.0 --abs" "q2"
+./movieq2.sh "$file" "q1 --vmin=0.0 --vmax=12.0 --colormap=seismic --abs" "q1"
+./movieq2.sh "$file" "q2 --vmin=0.0 --vmax=12.0 --colormap=seismic --abs" "q2"
 #./mov3eq2.sh "$file" "q3 --vmin=0.0 --vmax=24.0 --abs" "q3"
 ./movieq2.sh "$file" "rho --vmin=0.0 --vmax=1.0"
 ./movieq2.sh "$file" "rho --vmin=0.0 --vmax=1.0 -r 10" "ii"
