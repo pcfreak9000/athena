253782138f5ed74d689748a2ecaa2a0fe21415fb
pcfreak9000 <roman.borris@gmail.com>
2025-11-07 17:48:49 +0100

    added time averaged q1 and q2 plots to resolve script

--- resolve.sh
+++ resolve.sh
@@ -44,6 +44,8 @@ exshape 0.01
 echo "Generating additional plots..."
 vis/python/plot_spherical.py "$local_target_dir"/tavg.athdf rho --colormap=jet "$local_target_dir"/rho_tavg.png
 vis/python/plot_spherical.py "$local_target_dir"/tavg.athdf rho --colormap=jet --logc "$local_target_dir"/rho_tavg_log.png
+vis/python/plot_spherical.py "$local_target_dir"/tavg.athdf q1 --vmin=0.0 --vmax=12.0 --colormap=seismic --abs "$local_target_dir"/q1_tavg.png
+vis/python/plot_spherical.py "$local_target_dir"/tavg.athdf q2 --vmin=0.0 --vmax=12.0 --colormap=seismic --abs "$local_target_dir"/q2_tavg.png
 cd "$local_target_dir"
 gnuplot -e "set terminal png size 1000,1000; set output 'plot_dshapes.png'; set xrange[0:$x1max]; set yrange[0:$x1max]; plot \"dshape0.3-v.csv\" with line, \"dshape0.2-v.csv\" with line, \"dshape0.1-v.csv\" with line, \"dshape0.02-v.csv\" with line, \"dshape0.01-v.csv\" with line;" 
 gnuplot -e "set terminal png size 1000,1000; set output 'plot_qf.png'; set xrange[0:30000]; set yrange[0:20]; plot \"savg.csv\" using 1:2, \"savg.csv\" using 1:3, \"savg.csv\" using 1:4;"
